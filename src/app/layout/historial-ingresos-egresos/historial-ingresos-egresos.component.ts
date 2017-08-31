5ea1a539ca66877cd1291f22e1eba1300bcfc4af
Puchoo <fede.madoery@hotmail.com>
2017-08-31 16:58:05 -0300

    Historial con formato iterativo

--- src/app/layout/historial-ingresos-egresos/historial-ingresos-egresos.component.ts
+++ src/app/layout/historial-ingresos-egresos/historial-ingresos-egresos.component.ts
@@ -7,8 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistorialIngresosEgresosComponent implements OnInit {
 
-    historial: Array<any> = [];
-  constructor() { }
+    historial: [{
+        nroPatente: string,
+        montoAbonado: string,
+        horasEstacionadas: string,
+        fechaEstadia: string,
+        nombreCliente: string,
+        telefonoCliente: string
+    }];
+  constructor() {
+      this.historial = [{
+          nroPatente: 'AAA555',
+          montoAbonado: '54$',
+          horasEstacionadas: '2Hs',
+          fechaEstadia: '30/12/1874',
+          nombreCliente: 'Pedro',
+          telefonoCliente: '434343434'
+      },
+          {
+              nroPatente: 'AAA556',
+              montoAbonado: '54$',
+              horasEstacionadas: '2Hs',
+              fechaEstadia: '30/12/1874',
+              nombreCliente: 'Pedro2',
+              telefonoCliente: '434343434'
+          },
+          {
+              nroPatente: 'AAA557',
+              montoAbonado: '54$',
+              horasEstacionadas: '2Hs',
+              fechaEstadia: '30/12/1874',
+              nombreCliente: 'Pedro3',
+              telefonoCliente: '434343434'
+          },
+          {
+              nroPatente: 'AAA558',
+              montoAbonado: '54$',
+              horasEstacionadas: '2Hs',
+              fechaEstadia: '30/12/1874',
+              nombreCliente: 'Pedro4',
+              telefonoCliente: '434343434'
+          }];
+  }
 
   ngOnInit() {
   }
