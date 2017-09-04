690bee657a180ca5816350d4198f04fd9e6e33f6
nahuelgoldy <nahuelsg.64@gmail.com>
2017-09-04 10:41:42 -0300

    EgresoVehiculo (API), fix HistorialIngresos

--- src/app/layout/egreso-vehiculos/egreso-vehiculos.component.ts
+++ src/app/layout/egreso-vehiculos/egreso-vehiculos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {IngresoVehiculo} from '../../shared/domain/ingresoVehiculo';
+import {ParqueEstacionamiento} from '../../shared/domain/parqueEstacionamiento';
+import {ApiService} from '../../shared/services/api.service';
 
 @Component({
   selector: 'app-egreso-vehiculos',
@@ -6,19 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./egreso-vehiculos.component.scss']
 })
 export class EgresoVehiculosComponent implements OnInit {
+
+    ingreso: IngresoVehiculo = new IngresoVehiculo();
+    parque: ParqueEstacionamiento;
     closeAlert = false;
     submittedExitoso = false;
     submitted = false;
     nroPatente;
-  constructor() { }
+    patenteREGEX = /[a-z]{3}[0-9]{3}|[a-z]{2}[0-9]{3}[a-z]{2}/;
+
+  constructor(public apiService: ApiService) {
+      this.parque = JSON.parse(localStorage.getItem('currentParking'));
+  }
 
     ngOnInit() {
+    }
+
+    onPatenteChange() {
+      if (this.patenteREGEX.test(this.nroPatente)) {
+          this.apiService.get('ingreso/' + this.parque.idEstacionamiento + '/vehiculo/' + this.nroPatente)
+              .subscribe(json => {
+                  this.ingreso = json;
+              });
+      }
     }
 
     onSubmitClicked() {
         this.closeAlert = false;
         this.submittedExitoso = true;
         this.submitted = true;
+
+        this.apiService.put('ingreso/' + this.parque.idEstacionamiento + '/egresoVehiculo/' + this.nroPatente, null)
+            .subscribe(json => {
+                console.log(json);
+            });
+    }
+
+    private calcularCosto(): number {
+      const fechaIngreso = this.ingreso.fechaIngreso.replace('-', '/');
+      const cantHoras = Math.ceil((Date.now() - new Date(fechaIngreso).getMilliseconds()) / (60 * 60 * 1000));
+      return (cantHoras * this.parque.precioPorHora);
     }
 
 }
