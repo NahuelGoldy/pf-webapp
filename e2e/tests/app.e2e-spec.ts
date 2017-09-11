168b719842a89d0958945bc877ac6befd1624b3d
nahuelgoldy <nahuelsg.64@gmail.com>
2017-09-11 15:33:10 -0300

    Fix e2e testing Landing page, base URL

--- e2e/tests/app.e2e-spec.ts
+++ e2e/tests/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { SbAdminCliUpdatePage } from '../page-objects/app.po';
+import { LandingPage } from '../page-objects/app.po';
 
-describe('sb-admin-cli-update App', () => {
-  let page: SbAdminCliUpdatePage;
+describe('Donde estaciono? WEB - Landing page', () => {
+  let page: LandingPage;
 
   beforeEach(() => {
-    page = new SbAdminCliUpdatePage();
+    page = new LandingPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Donde estaciono? - WEB');
   });
 });
